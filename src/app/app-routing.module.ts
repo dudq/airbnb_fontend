e53463dd02c3e4f514965ddaef3bc45b02ccc1e5
NguyenVinh <nguyenquangvinh2518@gmail.com>
2020-01-15 10:22:47 +0700

    add path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import {RegisterComponent} from './components/user/register-user/register.compon
 import {RegisterHostComponent} from './components/host/register-host/register-host.component';
 import {LoginComponent} from './components/login/login.component';
 import {AddHouseComponent} from './components/host/add-house/add-house.component';
+import {ListHouseOfHostComponent} from './components/host/list-house-of-host/list-house-of-host.component';
+import {HomeListForGuestComponent} from './components/user/home-list-for-guest/home-list-for-guest.component';
 
 const routes: Routes = [
   {
@@ -22,6 +24,14 @@ const routes: Routes = [
   {
     path: 'add-house',
     component: AddHouseComponent
+  },
+  {
+    path: 'host/list-house-of-host',
+    component: ListHouseOfHostComponent
+  },
+  {
+    path: 'user/home-list-for-guest',
+    component: HomeListForGuestComponent
   },
   {
     path: 'register-host',
